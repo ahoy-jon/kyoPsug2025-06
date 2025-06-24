aef87d123ab721849d5a11efe7c624ad9516d971
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-06-25 01:19:26 +0200

    helloWorld!

---
+++ helloWorld.scala
@@ -0,0 +1,54 @@
+package helloWorld
+
+import java.io.IOException
+import kyo.*
+
+val init: Unit < Sync = Console.printLine("Hello!")
+
+val WHAT_IS_YOUR_NAME: String < (Sync & Abort[IOException]) =
+  Console.print("Please type your name? > ").andThen(Console.readLine)
+
+def hello(name: String): Unit < Sync =
+  Console.printLine(s"--- AHOY ${name.toUpperCase}! ---")
+
+//--- --- --- ---
+
+// map map map
+val program_0: Unit < (Sync & Abort[IOException]) =
+  init.map: Unit =>
+    WHAT_IS_YOUR_NAME.map: name =>
+      hello(name)
+
+// for-comp
+val program_1 =
+  for
+    _    <- init
+    name <- WHAT_IS_YOUR_NAME
+  yield hello(name)
+
+// single line
+val program_2 =
+  init.andThen(WHAT_IS_YOUR_NAME).map(hello)
+
+// direct-syntax
+val program_3 =
+  direct:
+    init.now
+    val name = WHAT_IS_YOUR_NAME.now
+    hello(name).now
+
+// point free?
+val program_4 =
+  init *> WHAT_IS_YOUR_NAME >>> hello
+
+//--- --- --- ---
+
+object HelloWorld extends KyoApp:
+  run:
+    program_3
+
+
+
+//Merci Valentin ! https://github.com/vil1/pointless, cela donne envie de faire kyo-pointless
+extension [A, S](v: A < S)
+  infix def >>> [B, S2](f: A => B < S2): B < (S & S2) = v.map(f)
---
+++ project.scala
@@ -0,0 +1,15 @@
+//> using scala 3.7.1
+
+//> using repository sonatype-s01:snapshots
+
+//> using dep io.getkyo::kyo-core:0.19.0+68-78580e5b-SNAPSHOT
+//> using dep io.getkyo::kyo-prelude:0.19.0+68-78580e5b-SNAPSHOT
+//> using dep io.getkyo::kyo-direct:0.19.0+68-78580e5b-SNAPSHOT
+//> using dep io.getkyo::kyo-combinators:0.19.0+68-78580e5b-SNAPSHOT
+
+//> using option -Wvalue-discard
+//> using option -Wnonunit-statement
+//> using option -Wconf:msg=(unused.*value|discarded.*value|pure.*statement):error
+//> using option -language:strictEquality
+
+
